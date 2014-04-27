092578e95b62f4313d86e035e0d2b9841d347c25
hmrakow <hmrakow@gmail.com>
2014-04-27 18:13:44 -0400

    updated query

--- js/app.js
+++ js/app.js
@@ -1,47 +1,62 @@
 // JavaScript Document
 
 $(document).ready(function(){
-	
-	//if list is empty let alert user
-	
-	
-	//add item to list
-	 $("button").on("click", getInput);
-	 
-	//add item to list triggered on Enter
-	$(document).on("keydown", function(e) {
-    	if(e.keyCode === 13) {
-       	 	getInput();
-    	}
-	}); 
- 
-	// Obtaining input and then calling addItem() with the input
-	function getInput() {
-    	var listInput = $(".listInput");
-    	var input = listInput.val();         	
-			addItem(input);    	
-	}
+	$("#addItemBtn").click (function(){
+		var content = $("#listInput").val();
+		var addItem = $('<div class="answersArea"></div>').text(my_text);
+		//add line through
+		addItem.on('click', function(){
+			$(this).toggleClass("lineThrough");
+		});
+		//remove item
+		addItem.on('dblclick', function(){
+			$(this).fadeOut('fast');
+		});
+		
+		//clear list
+		$(".clearListBtn").on('click', function(){
+			$(".answersArea").remove();		
+		});
 
-	function addItem() {     
-    	var content = "<div class=\"answersArea\">" + " " + "</div>";
-   		 $(".answersWrapper").append(content); 
-	}
-	
-	//remove object from list
-	$(".answersWrapper").dblclick(function(){
-		$(this).hide();
+
+		if (my_text!=""){
+			$("#answersWrapper").append(content);
+			$("#listInput").val("");
+		}
+
+		else{alert("Type an item in before you add it.")}
 	});
-	
-	//cross out item on list
-	$(".answersArea").on('click', function(){
-		$(this).addClass("lineThrough");		
+  
+  $(document).keypress(function(e){
+    if(e.which == 13){
+    	var content = $("#listInput").val();
+		var addItem = $('<div class="answersArea"></div>').text(my_text);
+			//add line through
+			addItem.on('click', function(){
+				$(this).toggleClass("lineThrough");
+			});
+			//remove item
+			addItem.on('dblclick', function(){
+				$(this).fadeOut('fast');
+			});
+			
+			//clear list
+			$(".clearListBtn").on('click', function(){
+				$(".answersArea").remove();		
+			});
+
+
+			if (my_text!="")
+			{
+				$("#answersWrapper").append(content);
+				$("#listInput").val("");
+			}
+			//warning to enter in content
+			else 
+			{
+				alert("Type an item in before you add it.")
+			}
+    }
 	});
-	
-	//clear list
-	$(".clearListBtn").on('click', function(){
-		$(".answersArea").remove();		
-	});
-	
-	//re-arrange items using jquery ui
-	
 });
+
